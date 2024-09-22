e3d9bf35106fd83aacf9a2c25f65b0e1596ca7dc
KJU3 <kju_3@naver.com>
2024-09-22 11:40:14 +0900

    feat: 결제 service 구현 (#20)

--- src/main/java/com/cm39/cm39/order/service/PaymentService.java
+++ src/main/java/com/cm39/cm39/order/service/PaymentService.java
@@ -1,7 +1,10 @@
 package com.cm39.cm39.order.service;
 
 import com.cm39.cm39.order.dto.PaymentDto;
+import com.cm39.cm39.order.dto.PaymentHandlerResponse;
 
 public interface PaymentService {
     int insertPayment(PaymentDto paymentDto);
+    void verifyPaymentRequest(String paymentKey, String orderId, Integer amount);
+    PaymentHandlerResponse confirmPayment(String paymentKey, String orderId, Integer amount);
 }
